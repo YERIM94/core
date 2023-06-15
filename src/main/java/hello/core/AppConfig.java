93692b8320b1003971e7835c593fbc719ee3a199
yerim94 <721yerim@gmail.com>
2023-06-15 21:36:13 +0900

    feat: apply ApplicationContext

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,28 +1,36 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig {
+@Configuration
+public class AppConfig { // 애플리케이션을 사용영역과 구성영역으로 분리
+
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 
-	private MemberRepository memberRepository() {
+	@Bean
+	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
-	private DiscountPolicy discountPolicy() {
-		return new FixDiscountPolicy();
+	@Bean
+	public DiscountPolicy discountPolicy() {
+		return new RateDiscountPolicy();
 	}
 }
